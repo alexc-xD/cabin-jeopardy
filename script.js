be4ca4bad4657f0169580fcc37c08ab5ed67732c
Adam <adammiller.2000nj@gmail.com>
2022-11-17 04:26:50 -0500

    added bunch of test stuff

---
+++ script.js
@@ -0,0 +1,8 @@
+let popup1001 = document.getElementById("popup")
+
+function openPopup() {
+    popup1001.classList.add("open-popup");
+}
+function closePopup() {
+    popup1001.classList.remove("open-popup");
+}
