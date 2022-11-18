7ad2a8895392d6ec26c782894132d121f6101e6a
Adam <adammiller.2000nj@gmail.com>
2022-11-18 00:27:08 -0500

    updated popups

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 let popup1001 = document.getElementById("popup")
 
 function openPopup() {
-    popup1001.classList.add("open-popup");
+    popup1001.classList.add("open-pop");
 }
 function closePopup() {
-    popup1001.classList.remove("open-popup");
+    popup1001.classList.remove("open-pop");
+}
+
+let popup1002 = document.getElementById("popup2")
+
+function openPopup2() {
+    popup1002.classList.add("open-pop2");
+}
+function closePopup2() {
+    popup1002.classList.remove("open-pop2");
+}
+
+let popup2001 = document.getElementById("popup3")
+
+function openPopup3() {
+    popup2001.classList.add("open-pop3");
+}
+function closePopup3() {
+    popup2001.classList.remove("open-pop3");
 }
