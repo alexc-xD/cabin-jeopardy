a685272b66af5a2836b06970e0bee0e2194482a7
Adam <adammiller.2000nj@gmail.com>
2022-11-22 16:22:56 -0500

    small interface changes

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let popup1001 = document.getElementById("popup")
+let popup1002 = document.getElementById("popup2")
+let popup1003 = document.getElementById("popup3")
+let popup1004 = document.getElementById("popup4")
+let popup2001 = document.getElementById("popup21")
+let popup2002 = document.getElementById("popup22")
 
 function openPopup() {
     popup1001.classList.add("open-pop");
@@ -7,8 +12,6 @@ function closePopup() {
     popup1001.classList.remove("open-pop");
 }
 
-let popup1002 = document.getElementById("popup2")
-
 function openPopup2() {
     popup1002.classList.add("open-pop2");
 }
@@ -16,11 +19,30 @@ function closePopup2() {
     popup1002.classList.remove("open-pop2");
 }
 
-let popup2001 = document.getElementById("popup3")
-
 function openPopup3() {
-    popup2001.classList.add("open-pop3");
+    popup1003.classList.add("open-pop3");
 }
 function closePopup3() {
-    popup2001.classList.remove("open-pop3");
+    popup1003.classList.remove("open-pop3");
+}
+
+function openPopup4() {
+    popup1004.classList.add("open-pop4");
+}
+function closePopup4() {
+    popup1004.classList.remove("open-pop4");
+}
+
+function openPopup21() {
+    popup2001.classList.add("open-pop21");
+}
+function closePopup21() {
+    popup2001.classList.remove("open-pop21");
+}
+
+function openPopup22() {
+    popup2002.classList.add("open-pop22");
+}
+function closePopup22() {
+    popup2002.classList.remove("open-pop22");
 }
