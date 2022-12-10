a6f55cfaad8590c1dec7807703ed81eb4dc0dccc
alexc-xD <alexandercacorrea@gmail.com>
2022-12-10 09:26:51 -0500

    wrote stuff 4 adam and commented it out

--- cabin-jeopardy-rework/script.js
+++ cabin-jeopardy-rework/script.js
@@ -36,3 +36,36 @@ function makeColumns(cellNum) {
 
   };
 };
+
+/*
+let score1 = 0
+let score2 = 0
+let score3 = 0
+
+function scoreUpdate(scorename, pointvalue) {
+
+  switch (scorename) { //takes the score part of the function call to know what to update
+
+    case "score1": //if you want to add more scores add more cases
+
+      score1 = score1 + pointvalue
+      if (score1 >= 9999 || score1 < 0) {
+        document.getElementById(scorename).innerHTML = pointvalue
+      }
+
+      else { } //write whatever error you want to give in {}
+
+    case "score2":
+
+      score2 = score2 + pointvalue
+      if (score2 >= 9999 || score2 < 0) {
+        document.getElementById(scorename).innerHTML = pointvalue
+      }
+
+      else { } //write whatever error you want to give in {}
+  }
+}
+
+//to use the function whenever you hit +- do for example scoreUpdate(score1,-100)
+
+*/
